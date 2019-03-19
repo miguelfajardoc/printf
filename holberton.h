827ebd997e770945c0fcb2f5ba50c556ec041e1c
miguelfajardoc <775@holbertonschool.com>
2019-03-19 00:56:23 +0000

    add 2 structures, the function structure and the position

--- holberton.h
+++ holberton.h
@@ -1,10 +1,20 @@
 #ifndef _PRINTF
 #define _PRINTF
-typedef struct t
+typedef struct typs
+{
+	char tip;
+	int pos;
+	void (*f)(va_list);
+} postipos;
+typedef struct tip
 {
 	char tipo;
-	int pos;
-} tipos;
-int look(const char *fomat);
+	void (*s)(va_list);
+} tiposf;
+void func(va_list vlist);
+void funs(va_list vlist);
+void fund(va_list vlist);
+int count(const char *format);
+int look(const char *fomat, postipos * position);
 int _printf(const char *format, ...);
 #endif
