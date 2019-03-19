cde7213a9261136519e105d81788ba307b3e0993
Mariana <marianaplazas@live.com>
2019-03-19 19:12:59 +0000

    the functions for char and strings, also the man and the paste function

---
+++ funs.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+int funs(int f, va_list vlist, va_list vlist2, *s)
+{
+	int i = 0;
+
+	if (f == 0)
+	{
+		i = _strenlen(va_arg(vlist, char *));
+		return(i);
+	}
+	else 
+	{
+		s = va_arg(vlist2, char *);
+		s = va_arg(vlist2, char *);
+		i = _strlen(s);
+		return(i);
+	}
+}
